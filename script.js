1d8efe568539a5d77e4d1ef9426ce09ae09611db
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-03-22 09:31:42 +0200

    another commit

--- script.js
+++ script.js
@@ -8,19 +8,28 @@
 // function arrayMethod(str1) {
 //   return str1.split("").reverse().join("");
 // }
+// function arrayMethod(str1) {
+//   const reversed = str1.replace(/\s/g, "").split("").reverse().join("");
+//   const original = str1.replace(/\s/g, "");
+//   if (original === reversed) {
+//     return "it is a palindrome";
+//   } else {
+//     return "it's not a palindrome";
+//   }
+// }
 function arrayMethod(str1) {
-  const reversed = str1.replace(/\s/g, "").split("").reverse().join("");
-  const original = str1.replace(/\s/g, "");
-  if (original === reversed) {
-    return "it is a palindrome";
-  } else {
-    return "it's not a palindrome";
-  }
+  const myArray = str1.toString().split("");
+  const lastNumber = parseInt(myArray[myArray.length - 1]);
+  if (myArray.length === 1 && lastNumber === 0)
+    return "it's not a multiple of five";
+  if (lastNumber === 0 || lastNumber === 5) {
+    return str1;
+  } else return "not a multiple of five";
 }
 
-console.log(arrayMethod("dede"));
-console.log(arrayMethod("race car"));
-console.log(arrayMethod("ana"));
+console.log(arrayMethod(5));
+console.log(arrayMethod(7565));
+console.log(arrayMethod(0));
 
 // console.log(arrayMethod(4, 2));
 
